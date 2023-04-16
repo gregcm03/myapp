81cfd2a4711ad87348a597f4d96949ced1406d0c
gregcm03 <unniat03@gmail.com>
2023-04-16 03:57:11 -0500

    bigupdate

--- src/Components/Income/Income.js
+++ src/Components/Income/Income.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useGlobalContext } from '../../context/globalContext';
 import { InnerLayout } from '../../styles/Layouts';
 
 function Income() {
+  const {addIncome} = useGlobalContext()
   return (
     <IncomeStyled>
         <InnerLayout>
@@ -10,7 +12,7 @@ function Income() {
           <div className="income-conent">
             <div className="form-container"></div>
             <div className="incomes">
-              
+
             </div>
           </div>
         </InnerLayout>
